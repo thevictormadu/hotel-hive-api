1bfb10213bcda131a682e25e51e8764d9f8aeec4
GbadamosiIE <100071560+GbadamosiIE@users.noreply.github.com>
2023-01-02 23:06:46 +0100

    Updated

---
+++ HotelManagement.Core/IServices/IRoomService.cs
@@ -0,0 +1,14 @@
+﻿using HotelManagement.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Core.IServices
+{
+    public interface IRoomService
+    {
+        Task<Response<GetRoomDto>> GetRoombyId(string Id);
+    }
+}
