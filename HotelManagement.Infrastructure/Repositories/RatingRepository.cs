cce1257a2e975841cecf3877325f4a5c9a149cff
victorpraiz <ivictorpraiz@gmail.com>
2023-01-06 15:49:17 +0100

    initial commit

---
+++ HotelManagement.Infrastructure/Repositories/RatingRepository.cs
@@ -0,0 +1,41 @@
+﻿using HotelManagement.Core.Domains;
+using HotelManagement.Core.DTOs;
+using HotelManagement.Core.IRepositories;
+using HotelManagement.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Infrastructure.Repositories
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly HotelDbContext context;
+        public RatingRepository(HotelDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<bool> RateHotel(RateHotelDTO request)
+        {
+            var hotel = await context.Hotels.FirstOrDefaultAsync(h => h.Id == request.HotelId);
+            if (hotel == null)
+            {
+                return false;
+            }
+            var rating = new Rating();
+            rating.Id = new Guid();
+            rating.CreatedAt = DateTime.Now;
+            rating.UpdatedAt = DateTime.Now;
+            rating.Ratings = request.Rating;
+            rating.HotelId = hotel.Id;
+            rating.Hotel = hotel;
+            await context.SaveChangesAsync();
+
+            return true;          
+        }
+    }
+}
---
+++ HotelManagement.Services/Services/HotelRatingService.cs
@@ -0,0 +1,43 @@
+﻿using HotelManagement.Core.Domains;
+using HotelManagement.Core.IRepositories;
+using HotelManagement.Core.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Services.Services
+{
+    public class HotelRatingService : IHotelRatingService
+    {
+
+        private readonly IRatingRepository ratingRepository;
+        public HotelRatingService(IRatingRepository ratingRepository)
+        {
+            this.ratingRepository = ratingRepository;
+        }
+
+        public Task<bool> RateHotel(int rate)
+        {
+            var check = false;
+            var ratings = ratingRepository.GetAllRating();
+            try
+            {
+                var rating = new Rating();
+                rating.Ratings = rate;
+                rating.CreatedAt = DateTime.Now;
+                rating.UpdatedAt = DateTime.Now;
+                
+                check = true;
+              
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return true;
+            
+        }
+    }
+}
