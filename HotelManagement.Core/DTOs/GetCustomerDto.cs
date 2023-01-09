31f27f4ed216ab050dc7aef0974cee6ee5cc575f
Success Imamun <successimamun@gmail.com>
2023-01-09 22:18:08 +0100

    created and included to dto for appuser

--- HotelManagement.Core/DTOs/GetCustomerDto.cs
+++ HotelManagement.Core/DTOs/GetCustomerDto.cs
@@ -12,7 +12,8 @@ namespace HotelManagement.Core.DTOs
         public string CreditCard { get; set; }
         public string Address { get; set; }
         public string State { get; set; }
-        public AppUser AppUser { get; set; }
+        public AppUserCustomerDto AppUserCustomerDto { get; set; }
+        //public AppUser AppUser { get; set; }
         //public ICollection<Booking> Bookings { get; set; }
         //public ICollection<WishList> WishLists { get; set; }
         //public ICollection<Review> Reviews { get; set; }
