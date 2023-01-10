faef0c45733da58747a6fc2a3b344693ce53afd7
sam-adroit <samuelbankole779@gmail.com>
2023-01-10 04:30:34 +0100

    Added unstaged/ignored files for ManagerRequest service

---
+++ HotelManagement.Core/DTOs/ManagerRequestDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Core.DTOs
+{
+    public class ManagerRequestDTO
+    {
+        public string ManagerName { get; set; }
+        public string HotelName { get; set; }
+        public string HotelAddress { get; set; }
+        public string Email { get; set; }
+    }
+}
