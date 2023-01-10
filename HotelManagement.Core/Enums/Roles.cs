fab8282cd03316445c401fd646ca4ea02caa8aaf
GbadamosiIE <100071560+GbadamosiIE@users.noreply.github.com>
2023-01-10 09:00:26 +0100

    Local

---
+++ HotelManagement.Core/Enums/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Core.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Roles
+    {
+        Admin=0,
+        Manager=1,
+        Customer=2,
+    }
+}
