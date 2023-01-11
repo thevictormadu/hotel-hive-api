3b6a089cf026873f383a4da54af2d6052382a22e
Samuel-olurotimi <samuel.olurotimi@decagon.dev>
2023-01-11 01:34:50 +0100

    Done WIth Hms 53

---
+++ HotelManagement.Infrastructure/Repositories/ManagerRepository.cs
@@ -0,0 +1,19 @@
+﻿using HotelManagement.Core.Domains;
+using HotelManagement.Core.IRepositories;
+using HotelManagement.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Infrastructure.Repositories
+{
+    public class ManagerRepository: GenericRepository<Manager>, IManagerRepository
+    {
+        public ManagerRepository(HotelDbContext hotelDbContext):base(hotelDbContext)
+        {
+                
+        }
+    }
+}
