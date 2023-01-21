b8128df8207427cd154903aa8b09e66a0900f995
victorpraiz <ivictorpraiz@gmail.com>
2023-01-21 19:47:10 +0100

    dev-hms-27 controller

---
+++ HotelManagement.Api/Controllers/RateHotelController.cs
@@ -0,0 +1,32 @@
+﻿using HotelManagement.Core.DTOs;
+using HotelManagement.Core.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RateHotelController : Controller
+    {
+        private readonly IRateHotelService _rateHotelService;
+
+        public RateHotelController(IRateHotelService rateHotelService)
+        {
+            _rateHotelService = rateHotelService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RateHotel([FromBody] RateHotelDTO rateHotelDto)
+        {
+            if (rateHotelDto == null)
+            {
+                return BadRequest("Invalid Input");
+            }
+
+            var result = await _rateHotelService.RateHotel(rateHotelDto);
+            return Ok(result);
+        }
+
+        // comment inside RateController
+    }
+}
