3d7b179c50ba44ff3aeb8e4f685f762e76720909
DORIS-DUNU <dunudoris3@gmail.com>
2023-01-31 11:15:04 -1000

    Added ignored file

---
+++ HotelManagement.Core/DTOs/GetCustomersByHotelDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Core.DTOs
+{
+    public class GetCustomersByHotelDto
+    {
+        public string  CustomerFirstName { get; set; }
+
+        public string CustomerLastName { get; set; }
+
+        public string EmailAddress { get; set; }
+        public string Gender { get; set; }
+
+        public string State { get; set; }
+
+    }
+}
